af5f335891b6fba2fa2fd33e396bac85af83f08d
dave <liucl@i-counting.cn>
2018-04-29 11:26:06 +0800

    [feat] init repository closed issue #1

---
+++ src/main/java/com/yuuyoo/springbootstarterlogging/SpringBootStarterLoggingApplication.java
@@ -0,0 +1,12 @@
+package com.yuuyoo.springbootstarterlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterLoggingApplication.class, args);
+	}
+}
